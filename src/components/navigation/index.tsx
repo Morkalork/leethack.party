9e7afee6e6a0d1a85c345611a66c8624b988d05d
Morkalork <maffelu@gmail.com>
2020-02-10 17:05:23 +0100

    Updated styling regarding currently selected navigational item

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import './index.scss';
 import {Link} from "react-router-dom";
+import {withRouter} from 'react-router-dom';
 
-export const Navigation = () => {
+interface Props {
+    location: {
+        pathname: string
+    }
+};
+
+const pathInfo = [{
+    path: '/',
+    text: 'Home'
+}, {
+    path: '/when',
+    text: 'When?'
+}];
+
+const getPaths = (currentPath: string) => pathInfo.map((info) => ({
+    path: info.path,
+    text: info.text,
+    selected: info.path === currentPath
+}))
+
+export const Navigation = withRouter((props: Props) => {
+    const paths = getPaths(props.location.pathname);
     return <nav>
         <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/when'>When?</Link></li>
+            {paths.map((path) => <li key={path.path}>
+                <Link to={path.path} className={path.selected ? 'selected' : ''}>
+                    {path.text}
+                </Link>
+            </li>)}
         </ul>
     </nav>
-}
+});
